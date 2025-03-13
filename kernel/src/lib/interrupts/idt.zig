d9a6d3998aede40757440ebc158fb5ce60ff4d90
Pratik Devkota <sudw1n@proton.me>
2025-03-13 16:03:48 +0545

    refactor(idt): rename field members/enum variants

--- kernel/src/lib/interrupts/idt.zig
+++ kernel/src/lib/interrupts/idt.zig
@@ -12,48 +12,49 @@ const SystemTableRegister = cpu.SystemTableRegister;
 
 /// Interrupt Descriptor Gate Type
 pub const GateType = enum(u4) {
-    Interrupt = 0b1110,
-    Trap = 0b1111,
+    Interrupt64 = 0b1110,
+    Trap64 = 0b1111,
 };
 
-pub const InterruptServiceRoutine = *const fn () noreturn;
+pub const InterruptServiceRoutine = *const fn () callconv(.Naked) noreturn;
 
 // TODO: consider encapsulating all of the global stuff in the IDT struct
 
 pub const InterruptDescriptor = packed struct {
     /// offset bits 0..15
-    offset_1: u16,
+    offset_low: u16,
     /// a code segment selector in GDT or LDT
     /// TODO: accept these values as parameters in init() rather than hardcoding
     selector: u16 = @intFromEnum(SegmentSelector.KernelCode),
     /// bits 0..2 holds Interrupt Stack Table offset, rest of bits zero.
     /// IST is used in combination with the TSS to force the cpu to switch stacks when handling a
     /// specific interrupt.
-    ist: u8 = 0,
+    ist: u3 = 0,
+    reserved1: u5 = 0,
     /// Gate type.
     // TODO: accept this value as a parameter in init() rather than hardcoding
-    gate_type: u4 = @intFromEnum(GateType.Interrupt),
-    /// Always zero.
-    zero: u1 = 0,
-    /// Privilege level.
+    gate_type: u4 = @intFromEnum(GateType.Interrupt64),
+    /// Reserved
+    reserved2: u1 = 0,
+    /// Privilege level allowed to access this interrupt.
     // TODO: accept this value as a parameter in init() rather than hardcoding
     dpl: u2 = @intFromEnum(Dpl.Kernel),
     /// Whether the gate is active.
     // TODO: set in init()
     present: u1 = 1,
     /// offset bits 16..31
-    offset_2: u16,
+    offset_mid: u16,
     /// offset bits 32..63
-    offset_3: u32,
+    offset_high: u32,
     /// reserved
-    reserved: u32 = 0,
+    reserved3: u32 = 0,
 
     pub fn init(isr_ptr: InterruptServiceRoutine) InterruptDescriptor {
         const isr: u64 = @intFromPtr(isr_ptr);
         return InterruptDescriptor{
-            .offset_1 = @as(u16, @truncate(isr)),
-            .offset_2 = @as(u16, @truncate(isr >> 16)),
-            .offset_3 = @as(u32, @truncate(isr >> 32)),
+            .offset_low = @as(u16, @truncate(isr)),
+            .offset_mid = @as(u16, @truncate(isr >> 16)),
+            .offset_high = @as(u32, @truncate(isr >> 32)),
         };
     }
 };
