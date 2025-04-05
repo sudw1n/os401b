2cb44d3c1a18659aac7a81f64593d1d943037c8e
Pratik Devkota <sudw1n@proton.me>
2025-04-05 14:47:48 +0545

    feat(pmm): arbitrary size allocations and frees

--- kernel/src/lib/memory/pmm.zig
+++ kernel/src/lib/memory/pmm.zig
@@ -15,10 +15,12 @@ const TOTAL_PAGES = blk: {
 };
 
 pub const PhysicalMemoryManager = struct {
+    pub const Error = error{
+        DoubleFree,
+    };
     // In our bitmap, each bit represents a page:
     //   0 -> reserved, 1 -> free
     free_bitmap: Bitmap,
-
     pub fn init(memmap_request: limine.MemoryMapRequest) PhysicalMemoryManager {
         // Attempt to obtain the memory map response from Limine.
         const memmap_response = memmap_request.response orelse @panic("Failed to get memory map response from Limine");
@@ -49,27 +51,70 @@ pub const PhysicalMemoryManager = struct {
             .free_bitmap = bitmap,
         };
     }
-    /// Allocate a single physical page.
-    pub fn alloc(self: *PhysicalMemoryManager) u64 {
-        // panicking here is fine because there are no physical pages to allocate so we can't
-        // recover from that
-        const freePage = self.free_bitmap.findFirstSet() orelse @panic("Out of memory");
-        self.free_bitmap.unset(freePage);
-        return pageToAddress(freePage);
+    /// Allocate pages that fit given size
+    pub fn alloc(self: *PhysicalMemoryManager, size: u64) []u8 {
+        // how many pages needed to fullfill this allocation
+        const pages_needed = std.math.divCeil(u64, size, PAGE_SIZE) catch {
+            @branchHint(.unlikely);
+            @panic("PhysicalMemoryManager.alloc(): Divide error");
+        };
+        const startPage = self.findContiguous(pages_needed);
+        // mark the range as reserved
+        self.free_bitmap.setRangeValue(.{ .start = startPage, .end = startPage + pages_needed }, false);
+        const bytes_needed = pages_needed * PAGE_SIZE;
+        const addr = self.pageToAddress(startPage);
+        const ptr: [*]u8 = @ptrFromInt(addr);
+        return ptr[0..bytes_needed];
+    }
+    // find a contiguous range of pages
+    fn findContiguous(self: *PhysicalMemoryManager, pages_needed: u64) u64 {
+        var candidate: u64 = 0;
+        // ensure candidate plus pages_needed is within bounds
+        while (candidate <= TOTAL_PAGES - pages_needed) {
+            var i: u64 = 0;
+            // check if all pages in the block starting at candidate are free
+            while (i < pages_needed) : (i += 1) {
+                if (!self.free_bitmap.isSet(candidate + i)) {
+                    // if page candidate + i is reserved, then we need to restart past that index
+                    candidate = candidate + i + 1;
+                    break;
+                }
+            }
+            if (i == pages_needed) return candidate;
+        }
+        @panic("Out of memory: contiguous block not found");
     }
     /// Deallocate the given physical page address.
     ///
     /// Returns false if the page is already free (double-free).
-    pub fn dealloc(self: *PhysicalMemoryManager, address: u64) bool {
-        if (!self.isFree(address)) {
-            return false;
+    pub fn free(self: *PhysicalMemoryManager, bytes: []u8) !void {
+        const size = bytes.len;
+        const pages = std.math.divCeil(u64, size, PAGE_SIZE) catch {
+            @branchHint(.unlikely);
+            @panic("PhysicalMemoryManager.alloc(): Divide error");
+        };
+        const address = @intFromPtr(bytes.ptr);
+        std.debug.assert(address % PAGE_SIZE == 0);
+        const startPage = addressToPage(address);
+        const endPage = startPage + pages;
+        std.debug.assert(endPage <= TOTAL_PAGES);
+        for (startPage..endPage) |i| {
+            if (self.free_bitmap.isSet(i)) {
+                @branchHint(.unlikely);
+                return Error.DoubleFree;
+            }
         }
-        const page = addressToPage(address);
-        self.free_bitmap.set(page);
-        return true;
+        self.free_bitmap.setRangeValue(.{ .start = startPage, .end = endPage }, true);
+    }
+    pub fn allocPage(self: *PhysicalMemoryManager) u64 {
+        // panicking here is fine because there are no physical pages to allocate so we can't
+        // recover from that
+        const freePage = self.free_bitmap.findFirstSet() orelse @panic("Out of memory: no single free page found");
+        self.free_bitmap.unset(freePage);
+        return pageToAddress(freePage);
     }
     /// Check if an address is free.
-    pub fn isFree(self: *PhysicalMemoryManager, address: u64) bool {
+    fn isFree(self: *PhysicalMemoryManager, address: u64) bool {
         const page = addressToPage(address);
         return self.free_bitmap.isSet(page);
     }
