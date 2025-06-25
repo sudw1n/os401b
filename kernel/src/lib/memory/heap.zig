47074a3f5d5e081fb1de5ee80695796ebfb681bf
Pratik Devkota <sudw1n@proton.me>
2025-06-25 18:49:34 +0545

    feat(memory/heap): use heap size from build options

--- kernel/src/lib/memory/heap.zig
+++ kernel/src/lib/memory/heap.zig
@@ -1,13 +1,16 @@
 const std = @import("std");
+const build_options = @import("build_options");
 const pmm = @import("pmm.zig");
 const paging = @import("paging.zig");
 
 pub var allocator: std.mem.Allocator = undefined;
 
 var fba: std.heap.FixedBufferAllocator = undefined;
+/// Heap size for the kernel, calculated as a fraction of the total memory specified in build options.
+pub const HEAP_SIZE: usize = (build_options.memory * 1024 * 1024) / 10;
 
-pub fn init(size: u64) void {
-    const frame = pmm.global_pmm.alloc(size);
+pub fn init() void {
+    const frame = pmm.global_pmm.alloc(HEAP_SIZE);
     const virt = paging.physToVirt(@intFromPtr(frame.ptr));
     paging.mapRange(virt, @intFromPtr(frame.ptr), frame.len, &.{
         .Writable,
