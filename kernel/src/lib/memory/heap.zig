c82217a54850995233debf027081d7b1a880a5f6
Pratik Devkota <sudw1n@proton.me>
2025-06-25 18:50:54 +0545

    feat(memory/heap): use optionals and initialize heap

--- kernel/src/lib/memory/heap.zig
+++ kernel/src/lib/memory/heap.zig
@@ -3,20 +3,18 @@ const build_options = @import("build_options");
 const pmm = @import("pmm.zig");
 const paging = @import("paging.zig");
 
-pub var allocator: std.mem.Allocator = undefined;
+var fba: ?std.heap.FixedBufferAllocator = null;
 
-var fba: std.heap.FixedBufferAllocator = undefined;
 /// Heap size for the kernel, calculated as a fraction of the total memory specified in build options.
 pub const HEAP_SIZE: usize = (build_options.memory * 1024 * 1024) / 10;
 
 pub fn init() void {
     const frame = pmm.global_pmm.alloc(HEAP_SIZE);
     const virt = paging.physToVirt(@intFromPtr(frame.ptr));
-    paging.mapRange(virt, @intFromPtr(frame.ptr), frame.len, &.{
-        .Writable,
-        .Present,
-    });
     const heap = @as([*]u8, @ptrFromInt(virt))[0..frame.len];
+    for (heap) |*byte| {
+        byte.* = 0; // Initialize the heap memory to zero
+    }
+    std.log.debug("Heap initialized at virtual address: {x:0>16}, size: {d}", .{ @intFromPtr(heap.ptr), heap.len });
     fba = std.heap.FixedBufferAllocator.init(heap);
-    allocator = fba.allocator();
 }
