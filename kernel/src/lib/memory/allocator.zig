cbafb160772bed058f3445b62e7744dbf8217586
Pratik Devkota <sudw1n@proton.me>
2025-07-07 17:49:06 +0545

    feat(memory/allocator): reuse freed memory in alloc()

--- kernel/src/lib/memory/allocator.zig
+++ kernel/src/lib/memory/allocator.zig
@@ -10,6 +10,7 @@ pub const Allocator = struct {
     remaining: usize,
 
     const ChunkHeader = struct {
+        // TODO: make this 0x10 bytes aligned, so that we can store the status in the first nibble
         size: usize,
         status: ChunkStatus,
     };
@@ -56,27 +57,45 @@ pub const Allocator = struct {
 
         var self: *Self = @ptrCast(@alignCast(ctx));
 
-        if (self.end_index + len > self.heap.len) {
-            return null;
+        const header_size = @sizeOf(ChunkHeader);
+        var hdr_ptr: *ChunkHeader = undefined;
+
+        // look for chunks already allocated and freed to find one that satisfies this allocation
+        var curr: usize = 0;
+
+        while (curr < self.end_index) {
+            hdr_ptr = @ptrCast(@alignCast(self.heap[curr..]));
+            if (hdr_ptr.size >= len and hdr_ptr.status == .Free) {
+                hdr_ptr.status = .Used;
+
+                const chunk = self.heap[curr + header_size ..];
+                self.remaining -= (len + header_size);
+                log.info("alloc@{x:0>16}:{x}, remaining {x}", .{ @intFromPtr(chunk.ptr), len, self.remaining });
+
+                return chunk.ptr;
+            }
+            curr += header_size + hdr_ptr.size;
         }
 
+        if (self.end_index + len > self.heap.len) return null;
+
         // typecast the current position to a ChunkHeader pointer
-        const hdr_ptr: *ChunkHeader = @ptrCast(@alignCast(self.heap[self.end_index..]));
+        hdr_ptr = @ptrCast(@alignCast(self.heap[self.end_index..]));
         // fill in the header information
         hdr_ptr.* = ChunkHeader{
             .size = len,
             .status = ChunkStatus.Used,
         };
         // move forward the pointer
-        self.end_index += @sizeOf(ChunkHeader);
+        self.end_index += header_size;
         // return the memory after the header
-        const chunk = self.heap[self.end_index .. self.end_index + len];
+        const chunk = self.heap[self.end_index..];
         // move forward the pointer by the size of allocation
         self.end_index += len;
 
-        self.remaining -= (len + @sizeOf(ChunkHeader));
+        self.remaining -= (len + header_size);
 
-        log.info("alloc@{x:0>16}:{x}, remaining {x}", .{ @intFromPtr(chunk.ptr), chunk.len, self.remaining });
+        log.info("alloc@{x:0>16}:{x}, remaining {x}", .{ @intFromPtr(chunk.ptr), len, self.remaining });
         return chunk.ptr;
     }
 
@@ -92,6 +111,7 @@ pub const Allocator = struct {
         const hdr_ptr: *ChunkHeader = ptrToHeader(memory.ptr);
         if (hdr_ptr.status == ChunkStatus.Used) {
             @branchHint(.likely);
+            log.debug("header@{x:0>16} = {any}", .{ @intFromPtr(hdr_ptr), hdr_ptr });
             hdr_ptr.status = ChunkStatus.Free;
             self.remaining += hdr_ptr.size + @sizeOf(ChunkHeader);
             log.info("free@{x:0>16}:{x}, remaining {x}", .{ @intFromPtr(memory.ptr), memory.len, self.remaining });
