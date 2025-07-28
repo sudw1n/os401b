f0a7bd815d008860b0dd1ef14213de3c6ea0ae7a
Pratik Devkota <sudw1n@proton.me>
2025-07-28 23:12:51 +0545

    feat(lock): use atomic instructions and impl interrupt handling locks

--- kernel/src/lib/lock.zig
+++ kernel/src/lib/lock.zig
@@ -1,19 +1,43 @@
+const std = @import("std");
+const registers = @import("registers.zig");
+
+const Rflags = registers.Rflags;
+const AtomicBool = std.atomic.Value(bool);
+
 pub const SpinLock = struct {
-    locked: bool = false,
+    locked: AtomicBool,
 
     pub fn init() SpinLock {
-        return SpinLock{};
+        return SpinLock{ .locked = AtomicBool.init(false) };
     }
 
     pub fn acquire(self: *SpinLock) void {
-        while (self.locked) {
-            // Busy-wait until the lock is available
-            asm volatile ("pause");
+        while (self.locked.cmpxchgWeak(
+            false, // expecting unlocked
+            true, // want to set locked
+            .acquire, // on success: act like an Acquire fence
+            .acquire, // on failure: fresh-load with Acquire semantics
+        ) != null) {
+            std.atomic.spinLoopHint();
         }
-        self.locked = true;
+    }
+
+    /// Acquire lock and disable interrupts,
+    /// returning the old RFLAGS so you can restore later.
+    pub fn acquireFlagsSave(self: *SpinLock) Rflags {
+        const flags = Rflags.get(Rflags);
+        asm volatile ("cli");
+        self.acquire();
+        return flags;
     }
 
     pub fn release(self: *SpinLock) void {
-        self.locked = false;
+        self.locked.store(false, .release);
+    }
+
+    /// Release lock and restore RFLAG
+    pub fn releaseFlagsRestore(self: *SpinLock, flags: Rflags) void {
+        self.release();
+        Rflags.set(Rflags, flags);
     }
 };
