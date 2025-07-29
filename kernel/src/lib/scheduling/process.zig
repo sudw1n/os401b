4dbb8fb5e6291e38ddd2d9a148985f9e29229e55
Pratik Devkota <sudw1n@proton.me>
2025-07-29 17:34:32 +0545

    feat(scheduling/process): use kernel's allocator

--- kernel/src/lib/scheduling/process.zig
+++ kernel/src/lib/scheduling/process.zig
@@ -120,10 +120,6 @@ pub const Thread = struct {
 
 /// A single process (or the idle task)
 pub const Process = struct {
-    /// The buffer used by the VMM Allocator for this process
-    vmm_buffer: []u8,
-    /// The allocator used by the VMM for this process.
-    vmm_allocator: heap_allocator.Allocator,
     /// The virtual memory manager for this process.
     vmm: VirtualMemoryManager,
 
@@ -148,14 +144,8 @@ pub const Process = struct {
 
     pub fn init(self: *Process, name: []const u8, pid: u64) void {
         log.info("Initializing process {s} (PID: {d})", .{ name, pid });
-        const heap_phys = pmm.global_pmm.alloc(VMM_BUFFER_SIZE);
-        const heap_virt = paging.physToVirt(@intFromPtr(heap_phys.ptr));
-        const heap = @as([*]u8, @ptrFromInt(heap_virt))[0..heap_phys.len];
-        @memset(heap, 0); // Initialize the heap memory to zero
 
-        var vmm_allocator = Allocator.initFixed(heap);
-        const allocator_instance = vmm_allocator.allocator();
-        const vmm_instance = VirtualMemoryManager.init(VIRT_BASE_START, VIRT_BASE_END, allocator_instance);
+        const vmm_instance = VirtualMemoryManager.init(VIRT_BASE_START, VIRT_BASE_END, heap_allocator.allocator());
 
         // The kernel lives entirely in the higher half (PML4 entries 256..511), and we want the kernel to exist in all
         // address spaces.
@@ -175,9 +165,6 @@ pub const Process = struct {
         @memcpy(vmm_instance.pt_root.entries[256..], vmm_lib.global_vmm.pt_root.entries[256..]);
 
         self.* = Process{
-            .name = [_]u8{0} ** NAME_MAX_LEN,
-            .vmm_buffer = heap,
-            .vmm_allocator = vmm_allocator,
             .name = undefined,
             .vmm = vmm_instance,
             .allocator = heap_allocator.allocator(),
@@ -186,12 +173,6 @@ pub const Process = struct {
 
         const len = @min(NAME_MAX_LEN, name.len);
         @memcpy(self.name[0..len], name[0..len]);
-        log.info("Process {s} (PID: {d}) initialized with VMM buffer at {x:0>16}:{x}", .{
-            self.name,
-            self.pid,
-            @intFromPtr(self.vmm_buffer.ptr),
-            self.vmm_buffer.len,
-        });
         @memset(self.name[len..], 0); // Null-terminate the name
     }
 
@@ -324,15 +305,7 @@ pub const Process = struct {
 
     pub fn deinit(self: *Process) void {
         log.info("Deinitializing process {s} (PID: {d})", .{ self.name, self.pid });
-        log.debug("Process deinit: vmm\n{}", .{self.vmm});
         self.vmm.deinit();
-        self.vmm_allocator.deinit();
-        // Clear the VMM buffer
-        log.debug("Process deinit: vmm buffer@{x:0>16}:{x}", .{ @intFromPtr(self.vmm_buffer.ptr), self.vmm_buffer.len });
-        @memset(self.vmm_buffer, 0); // Clear the VMM buffer
-        // Free the physical frame used for the VMM buffer
-        const vmm_buffer_phys = @as([*]u8, @ptrFromInt(paging.virtToPhys(@intFromPtr(self.vmm_buffer.ptr))))[0..self.vmm_buffer.len];
-        pmm.global_pmm.free(vmm_buffer_phys);
     }
 };
 
